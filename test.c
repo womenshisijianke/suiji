4f9b4909bbff6283085299fb57d4f2ffeeb9edd0
zhaojianjian <824703372@qq.com>
2014-05-05 09:54:38 +0800

    zjj

--- test.c
+++ test.c
@@ -4,13 +4,22 @@
 #include<stdlib.h>
 int main()
 {
-    int i,j,m=0,flag=0,a[40];
-    srand((int)time(0));
+    int i,j,m=0,n=0,a[40];
      for(i=0;i<=39;i++)
+      a[i]=i;
+	 srand((int)time(NULL));   
+	 while(m<=39)
      {
       j=rand()%40;
-       { printf("%d-%s",info[j].id,info[j].name);m++;
-         if(m%4==0)  printf("\n");
+      for(i=0;i<40;i++)
+	
+      if(j==a[i])
+       {  m++; 
+		   a[i]=40;
+		   if(m%4==1)
+		   printf("第%d组:\n",++n);
+		 printf("%d-%s\n",info[j].id,info[j].name);
+        // if(m%4==0)  printf("\n");
        }
       }
 	return 0;
